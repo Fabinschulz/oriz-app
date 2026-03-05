5a5d05bd270cb3aa4c71c4703f048f989cd885d7
fabio lima <fabio.lima19997@gmail.com>
2026-03-05 00:08:11 -0300

    fix: configure android signing and fix MainActivity package structure

--- android/app/src/main/kotlin/com/example/oriz_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.oriz
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/oriz/com/MainActivity.kt
@@ -0,0 +1,5 @@
+package oriz.com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
